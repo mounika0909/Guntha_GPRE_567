8b27cc17d46fabb8c2b94d2e67787de650b18c8c
mounika0909 <30504951+mounika0909@users.noreply.github.com>
2017-09-16 12:18:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+	
+		Scanner sc=new Scanner(system.in);
+		Student newStudent=new Student();
+		int size;
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
